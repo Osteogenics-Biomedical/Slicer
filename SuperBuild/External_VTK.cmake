80f24aa662e6ecfa9ac277a9cf1b879f09470689
Connor Bowley <connor.bowley@kitware.com>
2022-09-08 09:40:45 -0400

    ENH: Update VTK
    VTK Changes:
    - Bug fix in vtkImprintFilter

--- SuperBuild/External_VTK.cmake
+++ SuperBuild/External_VTK.cmake
@@ -139,7 +139,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
   set(_git_tag)
   if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "9")
-    set(_git_tag "774445243fac1435bdcb8cc0b2cf918e8229d8ad") # osteogenics-v9.1.20220125-efbe2afc2
+    set(_git_tag "0b0e323e2a3dc7f9133f06541416026bb6ec2708") # osteogenics-v9.1.20220125-efbe2afc2
     set(vtk_egg_info_version "9.1.20220125")
   else()
     message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
